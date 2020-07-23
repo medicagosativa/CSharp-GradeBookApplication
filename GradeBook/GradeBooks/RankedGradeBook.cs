45b835a5b6ffcd24d20f5ea88ed2613eeb02aad5
Kev <flueckigerkevin@outlook.com>
2020-07-23 21:23:12 +0200

    csharp project part 2

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,13 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public GradeBookType Type;
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,13 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public GradeBookType Type;
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
